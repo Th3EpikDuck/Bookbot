73b57a1631c73f7cce445e249be1c9e33cddc2bf
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-05 02:51:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
       return f.read()
@@ -5,7 +7,7 @@ def get_book_text(filepath):
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count = word_count(text)
+    count = get_num_words(text)
     print(f"Found {count} total words")
 
 main()
