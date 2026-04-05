3ee158851b351a2b17b4f222dd9ae991812d8faa
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-05 03:10:48 -0700

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     new_text = text.split()
     return len(new_text)
+
+def get_char_count(text):
+    chars = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
