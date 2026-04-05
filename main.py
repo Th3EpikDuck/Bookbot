1dff2f8523617d72caadaa9302835659ad024f1e
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-05 02:05:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def get_book_text(filepath):
     with open(filepath) as f:
       return f.read()
 
+def word_count(text):
+    new_text = text.split()
+    return len(new_text)
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    word_count = word_count(text)
+    print(f"Found {word_count} total words")
 
 main()
