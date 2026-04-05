f16df6b6285981a61dbf437ca5a7721f6cb5d0eb
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-05 08:35:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_num_words
 from stats import get_char_count
 from stats import sort_chars
@@ -7,7 +9,11 @@ def get_book_text(filepath):
       return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+        
     text = get_book_text(book_path)
 
     print("============ BOOKBOT ============")
