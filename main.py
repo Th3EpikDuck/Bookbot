ff57a6ceb75bad5e2c6b4912cac9967cc9ce17ce
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-05 04:48:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_char_count
+from stats import sort_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -12,5 +13,7 @@ def main():
     print(f"Found {count} total words")
     char_count = get_char_count(text)
     print(char_count)
-
+    sorted_chars = sort_chars(text)
+    print(sorted_chars)
+    
 main()
