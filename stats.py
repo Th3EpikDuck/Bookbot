8799a0e62e4e6d54bb31167e432939814e1ce308
Th3EpikDuck <th3epikduck@gmail.com>
2026-04-05 07:21:18 -0700

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,7 +1,6 @@
 def get_num_words(text):
-    new_text = text.split()
-    return len(new_text)
-
+    return len(text.split())
+    
 def get_char_count(text):
     chars = {}
     for char in text:
@@ -12,9 +11,14 @@ def get_char_count(text):
             chars[lowered] = 1
     return chars
 
-def sort_chars(text):
+def sort_on(item):
+    return item["num"]
+
+def sort_chars(char_dict):
     sorted_list = []
+
     for char, count in char_dict.items():
         sorted_list.append({"char": char, "num": count})
-    sorted_list.sort(key=get_num, reverse=True)
+
+    sorted_list.sort(key=sort_on, reverse=True)
     return sorted_list
